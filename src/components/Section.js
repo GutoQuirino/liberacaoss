40103096c91196ffd51232de3c1e31b8bf9f5067
GustavoQuirino <gh.quirino@yahoo.com>
2022-09-20 15:31:31 -0300

    criado tela

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,8 +6,10 @@ const Section = () => {
         <div className="tela">
             <div className="janela">
                 <h1>CHAVE CLIENTE</h1>
-                <input type='text'placeholder="00000"/>
+                <input type='text'placeholder="00000" maxLength={5}/>
+                <button>GERAR</button>
                 <p>NUMERO DE LIBERACAO</p>
+                <h1></h1>
             </div>
         </div>
 
