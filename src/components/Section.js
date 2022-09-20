cd7bfc4d7cc5b116cb6eeda916303ff802cebb07
GustavoQuirino <gh.quirino@yahoo.com>
2022-09-20 19:40:44 -0300

    aplicado funcao

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,14 +3,31 @@ import {useState} from "react"
 
 const Section = () => {
 
+    let d = new Date();
+    let ano = d.getFullYear();
+    let mes = d.getMonth();
+    let dia = d.getDate();
+    let somaAno = (ano - 1900)*365;
+    let bisexto = (ano - 1900)/4;
+    let somaMes = (mes)*30; 
+    let result =(somaAno + somaMes + dia + bisexto)+ 1;
+    let codDia = result.toFixed(0);
+
     const [chave,setChave] = useState()
     const [numlib, setNumlib] = useState()
 
     const getChave = (e) => {
-        setChave(e.target.value)
+        setChave(`0${e.target.value}`)
     }
     const gerar = () => {
-        setNumlib(`000${chave}000`)
+        let resultado = parseInt(codDia) +30;
+        let codigos = '0'+resultado.toString();
+        let cod1 = codigos.slice(0,3);//codigo de variaçao parte 1
+        let cod2 = codigos.slice(3);//codigo de variaçao parte 2
+        let chCliente1 =chave.substr(0,3) ;
+        let chCliente2 =chave.substr(3,3) ;
+        let numRenovacao = '000'+cod2+chCliente1+cod1+chCliente2+'000';
+        setNumlib(numRenovacao)
     }
     const limpa = (e) => {
         e.target.value = ''
