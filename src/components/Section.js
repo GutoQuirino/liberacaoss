104c643bbdc919a3a4052adea31dac6aa157f91a
GustavoQuirino <gh.quirino@yahoo.com>
2022-09-20 15:13:26 -0300

    criado janela

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,11 @@ const Section = () => {
 
     return (
         <div className="tela">
-
+            <div className="janela">
+                <h1>CHAVE CLIENTE</h1>
+                <input type='text'placeholder="00000"/>
+                <p>NUMERO DE LIBERACAO</p>
+            </div>
         </div>
 
     )
