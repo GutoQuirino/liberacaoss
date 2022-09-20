be44c07d7470ea5389e84f7ce860326d19c1db34
GustavoQuirino <gh.quirino@yahoo.com>
2022-09-20 19:13:14 -0300

    v1.0

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,15 +1,30 @@
 import Whats from "./Whats";
+import {useState} from "react"
 
 const Section = () => {
+
+    const [chave,setChave] = useState()
+    const [numlib, setNumlib] = useState()
+
+    const getChave = (e) => {
+        setChave(e.target.value)
+    }
+    const gerar = () => {
+        setNumlib(`000${chave}000`)
+    }
+    const limpa = (e) => {
+        e.target.value = ''
+        setNumlib('')
+    }
 
     return (
         <div className="tela">
             <div className="janela">
                 <h1>CHAVE CLIENTE</h1>
-                <input type='text'placeholder="00000" maxLength={5}/>
-                <button>GERAR</button>
+                <input type='text'placeholder="00000" maxLength={5} onChange={getChave} onClick={limpa}/>
+                <button onClick={gerar}>GERAR</button>
                 <p>NUMERO DE LIBERACAO</p>
-                <h1></h1>
+                <h1>{numlib}</h1>
             </div>
             <Whats/>
         </div>
--- src/components/Whats.js
+++ src/components/Whats.js
@@ -1,9 +1,14 @@
-
+import {FaWhatsapp} from "react-icons/fa"
 const Whats = () => {
 
     return (
         
-        <a href="https://wa.me/551144936100" className="whats"><div >Precisa de ajuda ?</div></a>
+        <a href="https://wa.me/551144936100" className="whats">
+            <div className="btn-whats" >
+                <FaWhatsapp/>
+                <h3>Precisa de ajuda ?</h3>
+            </div>
+        </a>
     )
 
 }
